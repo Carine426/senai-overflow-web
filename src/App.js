847fe5bce3ff052a4c66a9a5dbb1ae603843727b
Carine426 <carinef082@gmail.com>
2021-08-16 11:25:50 -0300

    Atualizado

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-function Page1(){
-  return <h1>Está é a página 1</h1>;
-}
-
-function Page2(){
-  return <h1>Está é a página 1</h1>;
-}
-
+import { GlobalStyles } from "./GlobalStyles";
+import Home from "./pages/Home";
 
 function App() {
 
-  let currentPage = 2;
 
   return (
-    <div className="App">
-      {currentPage === 1 ? <Page1 /> : <Page2 />}
-    
-    </div>
+    <>
+    <GlobalStyles/>
+   <Home/>
+   </>
   );
 }
 
